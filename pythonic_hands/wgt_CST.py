0d666f70f882ed56fa2910f37984f8d5e82f8b0e
endurrus <endurrus@gmail.com>
2021-02-03 04:53:24 +0000

    my behavior

---
+++ pythonic_hands/wgt_CST.py
@@ -0,0 +1,46 @@
+import random
+import numpy as np
+import random
+
+def wgt_cst(user_id):
+  if get_field(user_id, "curr_energy") >= 40:
+    change(user_id, "curr_energy", -40)
+    gacha(user_id, get_field(user_id, 'item_list'))
+  else:
+  '''javier's handjob time'''
+    pass
+    
+def gacha(user_id, item_list):
+  list1 = ['Pythonic Hands', 'Computational Genius', 'SMS Scholar']
+  list2 = ['Loyalty to Country', 'Leadership', 'Discipline', 'Proffessionalism', 'Fighting Spirit', 'Ethics', 'Care for Soldiers', 'Safety']
+  d = {'SSR' : list1, 'SR' : list2}
+  items = ['SSR', 'SR', "Guess I'll study again another time!"] # Item list
+  prob = [0.5, 0.49, 0.01] # list of probabilities (sum should be 1)
+  N = 1 # Number of draws, always 01
+  #Lottery
+  result = (np.random.choice(items, p=prob))
+  
+  if result == "SSR":
+    a = random.randint(0, len(list1)-1)
+    if item_list[a] == '1':
+      return gacha(user_id, item_list)
+      
+    else:
+      print(a) # BINARY INT THAT NEEDS TO BE CHANGED
+      print(d[result][a]) # PRIZE WON, PRINT IT OUT, javier
+      change(user_id, 'item_list', a)
+      
+  elif result == "SR":
+    a = random.randint(0, len(list2)-1)
+    if item_list[a] == '1':
+      return gacha(user_id, item_list)
+
+    else:
+      print(3+a) # BINARY INT THAT NEEDS TO BE CHANGED
+      print(d[result][a]) # PRIZE WON, PRINT IT OUT, javier
+      change(user_id, 'item_list', 3+a)
+
+  else:
+    # print result, nothing happens
+    print(result)
+    # javier blowjob
