a95bcdc2dda764b516d7aa946e1f5509f6d9755d
endurrus <endurrus@gmail.com>
2021-02-03 05:26:27 +0000

    quotes for work/training

---
+++ pythonic_hands/work_training_quotes.py
@@ -0,0 +1,8 @@
+workquote1 = ['Flipping through files...', 'Connecting to I-Net...', 'Formatting PPTX slides...', 'Typing a report...', 'Presenting to HHQ...', 'Raising the flags...', 'Jogging around the camp...']
+workquote2 = ['Chiding understudy...', 'Greeted OC...', 'Greeted CO...', 'Doing saikang...', 'Taking a break...', 'Drinking some coffee...', 'Greeted LTA Algernon...', 'Saw Encik Zhi Peng...']
+workquote3 = ['Polishing boots...', 'Looking at the clock...', 'Lowering the flags...', 'Said bye to CPT Rayton...', 'Said bye to ME4 Min Yang...', 'Walking to the guardhouse...']
+
+trainingquote1 = ['Reading LPTHW...', 'Reading CCNA materials...', 'Googling stack overflow...', 'Reading Python Libraries...', 'Looking up Scapy notes....', 'Playing with Debian...', 'Downloading Kali...']
+trainingquote2 = ['Looking for Encik Muru...', 'Asking CPT Rayton...', 'Finding CPT Wen Zong...', 'Texting ME4 Min Yang...', 'Texting ME4T Javier...', 'Asking ME4T Zhi Qin...', 'Dialing up poly lecturers...', 'Thinking hard...']
+trainingquote3 = ['Googling OOP...', 'Looking up regex...', 'Asking on stack overflow...', 'Visiting hackerrank.com...', 'Entering learnpython.org...', 'Still thinking hard...']
+
