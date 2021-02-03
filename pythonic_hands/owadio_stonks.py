cf1f269bae4219bbbfae56e5ab15713b553c9bb2
lohzq88 <78394586+lohzq88@users.noreply.github.com>
2021-02-03 17:04:14 +0800

    Update owadio_stonks.py

--- pythonic_hands/owadio_stonks.py
+++ pythonic_hands/owadio_stonks.py
@@ -1,5 +1,3 @@
-import math
-
 def stonks(user_id):
 	# Display price of stonks for today comparative to ytd.
 	# Prompt Buy or Sell stonks
@@ -19,33 +17,22 @@ def stonks(user_id):
 	    print('Cfm want Buy?')
 	    b = input()
 	    if b == 'Yes':
-	      change_field(user_id, 'stonks_count', +(int(a)))
-	      change_field(user_id, 'curr_balance', (int(a)*int(b)))
+	      change_field(user_id, 'stonks_count', (int(a)))
+	      change_field(user_id, 'curr_balance', -(int(a)*int(b)))
 	      print(f"Bought {a} stonks at {stonk_price} for a total of {a*b}.")
-      else:
-        break
-    else:
-      break
-  else:
-    break
 
-  # Sell stonks code
-  if input() == sellstonk:
-    print('Sell how many?')
-	  a = input()
-	  if a.isdigit() and int(a)*stonk_price <= stonks_count:
-	    print('Cfm want Sell?')
-	    b = input()
-	    if b == 'Yes':
-	      change_field(user_id, 'stonks_count', -(int(a)))
-	      change_field(user_id, 'curr_balance', (int(a)*int(b)))
-	      print(f"Sold {a} stonks at {stonk_price} for a total of {a*b}.")
-      else:
-        break
-    else:
-      break
-  else:
-    break
-  # javier hands to change in the future, ideally ask for input again
-  # input validity not done, and a "fuck go back" option not done
+	# Sell stonks code
+	if input() == sellstonk:
+		print('Sell how many?')
+		a = input()
+		if a.isdigit() and int(a)*stonk_price <= stonks_count:
+			print('Cfm want Sell?')
+			b = input()
+			if b == 'Yes':
+				change_field(user_id, 'stonks_count', -(int(a)))
+				change_field(user_id, 'curr_balance', (int(a)*int(b)))
+				print(f"Sold {a} stonks at {stonk_price} for a total of {a*b}.")
+      
+		# javier hands to change in the future, ideally ask for input again
+		# input validity not done, and a "fuck go back" option not done
 stonks()
